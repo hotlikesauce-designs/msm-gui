ee891c26b2eed86dd116d80431bc63dd0aa51b1f
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-11-08 14:21:16 +0000

    added form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,16 @@
 class MoviesController < ApplicationController
+  
+  def create
+    # params hash looks like: 
+    # retrieve user's input from params
+    # create a record in the movie table
+    # populate each column with the users input
+    # Save
+    
+    # Redirected the user back to the /movies url
+
+  end
+  
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
