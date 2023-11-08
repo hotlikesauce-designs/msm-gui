6b1a7c402d14930607e43f686acfa8d211a34e30
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-11-08 15:13:26 +0000

    able to create movie entries

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,19 @@
 class MoviesController < ApplicationController
   
   def create
-    # params hash looks like: 
+    # Parameters: {"the_title"=>"1", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>""}
+
+    m = Movie.new
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.image = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+    m.save
+
+    redirect_to("/movies")
+
     # retrieve user's input from params
     # create a record in the movie table
     # populate each column with the users input
