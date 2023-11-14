ce3df98491f8c62bb3ae7e951e58ecde3d6204ca
hotlikesauce-designs <146861584+hotlikesauce-designs@users.noreply.github.com>
2023-11-14 21:03:54 +0000

    update movies working, 70% grade

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -31,6 +31,30 @@ class MoviesController < ApplicationController
     redirect_to("/movies")
   end
   
+  def update
+    #  Parameters: {"the_title"=>"1", "the_year"=>"2023", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>"6", "an_id"=>"1"}
+
+    # get the ID out of params
+    m_id = params.fetch("an_id")
+    
+    # look up the existing record
+    matching_records = Movie.where({:id => m_id})
+    the_movie = matching_records.at(0)
+    
+    # overwrite each column with user values
+    the_movie.title = params.fetch("the_title")
+    the_movie.year = params.fetch("the_year")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+    
+    # save and redirect 
+    the_movie.save
+    redirect_to("/movies/#{the_movie.id}")
+    
+  end
+
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
